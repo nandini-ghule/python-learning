27ff59de69a450914060aaae970007a4830ea87b
Nandini Ghule <nandinighule06@gmail.com>
2026-09-21 20:51:20 +0530

    Added program to check armstrong number

---
+++ armstrong.py
@@ -0,0 +1,15 @@
+num = int(input("Enter a number: "))
+
+original = num
+digits = len(str(num))
+sum = 0
+
+while num > 0:
+    digit = num % 10
+    sum = sum + digit ** digits
+    num = num // 10
+
+if sum == original:
+    print(original, "is an Armstrong number")
+else:
+    print(original, "is not an Armstrong number")
