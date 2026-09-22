03b80ccb97f6cb52226e2dd39e67ca75f8abc420
Nandini Ghule <nandinighule06@gmail.com>
2026-09-22 22:35:51 +0530

    Added odd even checker

---
+++ odd_even.py
@@ -0,0 +1,5 @@
+number_to_check = int(input("What number do you want to check?"))
+if number_to_check % 2 == 0:
+    print(number_to_check, "is an even number.")
+else:
+    print(number_to_check, "is an odd number.")
