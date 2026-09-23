3984110caa285c6311cb8206af00d674df91d1ea
Nandini Ghule <nandinighule06@gmail.com>
2026-09-23 23:17:53 +0530

    Added program to split sentence

---
+++ split_sentence.py
@@ -0,0 +1,7 @@
+sentence = input("Enter a sentence: ")
+
+#using the split() method to split the sentence into words
+words = sentence.split()
+
+print("Words:", words)
+print("Number of words:", len(words))
